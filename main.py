48af4e12d8adb476cb3ac6c1eb0797d7ddb65bed
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-01-11 09:07:22 -0500

    groups view 👁

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ def groups():
     groups_creator = Groups.query.filter_by(author_id=user_id).all()
     context = {
         "user_id": user_id,
-        'groups_creator': groups_creator
+        "groups_creator": groups_creator
     }
     return render_template('groups/index.html', **context)
 
