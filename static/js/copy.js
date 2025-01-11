327680c6c4731a3d53119922cc375d14b0518975
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-01-11 10:30:04 -0500

    secret code 💻

---
+++ static/js/copy.js
@@ -0,0 +1,9 @@
+function copyToClipboard(code) {
+    const textarea = document.createElement('textarea');
+    textarea.value = code;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert("¡Contenido copiado al portapapeles!");
+}
